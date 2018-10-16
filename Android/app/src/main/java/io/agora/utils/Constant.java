f01aa1f6095ceeff0ad26e03ebbf28c909cab665
brianxcli <neteasy1210@gmail.com>
2018-10-16 15:11:46 +0800

    Add three more sound effects

--- Android/app/src/main/java/io/agora/utils/Constant.java
+++ Android/app/src/main/java/io/agora/utils/Constant.java
@@ -28,7 +28,7 @@ public class Constant {
     public static final String ACTION_KEY_ROOM_NAME = "ecHANEL";
     public static final String ACTION_KEY_TITLE_NAME = "eTITLE";
 
-    public static String[] SOUNDARRAY = new String[]{"大叔","正太","猪八戒","空灵","浩克","萝莉"};
+    public static String[] SOUNDARRAY = new String[]{"大叔","正太","猪八戒","空灵","浩克","萝莉","电台KTV","演唱会","录音棚"};
 
 
 }
